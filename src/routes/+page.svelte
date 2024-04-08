eb26c6ef2f24805b917fa6e1e98fbb1a505861d5
pebloop <pebloop@outlook.com>
2024-04-08 19:02:23 +0200

    Fix auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-	import { SignIn, SignOut } from "@auth/sveltekit/components"
-	import { page } from "$app/stores"
+	import { signIn, signOut } from "@auth/sveltekit/client"
 	import logo from '$lib/images/logo.svg';
+
+	let password = "";
 </script>
 
 <svelte:head>
@@ -16,26 +17,10 @@
 		</picture>
 	</h1>
 
-	<div>
-		{#if $page.data.session}
-			{#if $page.data.session.user?.image}
-				<img
-						src={$page.data.session.user.image}
-						class="avatar"
-						alt="User Avatar"
-				/>
-			{/if}
-			<span class="signedInText">
-      <small>Signed in as</small><br />
-      <strong>{$page.data.session.user?.name ?? "User"}</strong>
-    </span>
-			<SignOut>
-				<div slot="submitButton" class="buttonPrimary">Sign out</div>
-			</SignOut>
-		{:else}
-			<span class="notSignedInText">You are not signed in</span>
-			<SignIn provider="google"/>
-		{/if}
+	<div class="actions">
+		<div class="wrapper-form">
+			<button on:click={() => signIn()}>Sign In </button>
+		</div>
 	</div>
 </section>
 
