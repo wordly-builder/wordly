9faa7d25cd27a53da9e80c91e3de148c5256df81
pebloop <pebloop@outlook.com>
2024-04-09 14:49:57 +0200

    Add drizzle and supabase

---
+++ src/lib/database/index.ts
@@ -0,0 +1,12 @@
+import "server-only";
+
+import { drizzle } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
+import { SUPABASE_URL } from "$env/static/private"
+
+const client = postgres(SUPABASE_URL, { onnotice: () => null });
+
+export const db = drizzle(client);
+
+await migrate(db, { migrationsFolder: "db/migrations" });
