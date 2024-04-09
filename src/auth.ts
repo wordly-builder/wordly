a171761ee1fd81e5fbbf9ec9a1554393a2902faa
pebloop <pebloop@outlook.com>
2024-04-09 15:32:41 +0200

    Update db management

--- src/auth.ts
+++ src/auth.ts
@@ -2,11 +2,10 @@ import { SvelteKitAuth } from "@auth/sveltekit"
 import Google from "@auth/core/providers/google"
 import { GOOGLE_ID, GOOGLE_SECRET, SUPABASE_API_URL, SUPABASE_SECRET } from "$env/static/private"
 import {SupabaseAdapter} from "@auth/supabase-adapter";
+import {DrizzleAdapter} from "@auth/drizzle-adapter";
+import {db} from "./lib/database";
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
     providers: [Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })],
-    adapter: SupabaseAdapter({
-        url: SUPABASE_API_URL,
-        secret: SUPABASE_SECRET
-    })
+    adapter: DrizzleAdapter(db)
 })
