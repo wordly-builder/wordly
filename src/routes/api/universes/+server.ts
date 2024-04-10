1914973bc538a7563f7d067fdab8724975fecff5
pebloop <pebloop@outlook.com>
2024-04-10 14:03:57 +0200

    Add universe creation

--- src/routes/api/universes/+server.ts
+++ src/routes/api/universes/+server.ts
@@ -5,16 +5,19 @@ export async function POST({ request }: { request: Request }) {
     const {name, session} = await request.json();
 
     if (!session || !session.user || !session.user.id) {
+        console.log('no session');
         throw error(401, 'Unauthorized');
     }
 
     const account = await database.auth.getAccountByUserId(session.user.id);
     if (account.length === 0) {
+        console.log('no account');
         throw error(401, 'Unauthorized');
     }
 
     const profile = await database.profiles.getByGoogleId(account[0].providerAccountId);
     if (profile.length === 0) {
+        console.log('no profile');
         throw error(401, 'Unauthorized');
     }
 
