de0478dfd6cb619d21c1e0dc429279d20232e2b3
Pebloop <pebloop@outlook.com>
2024-11-17 20:56:43 +0100

    Update drizzle migration files

--- .drizzle/0001_steep_slayback.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE "character_fields";--> statement-breakpoint
-DROP TABLE "characters_panel";--> statement-breakpoint
-DROP TABLE "characters_templates_field";--> statement-breakpoint
-DROP TABLE "characters_template";--> statement-breakpoint
-DROP TABLE "character";--> statement-breakpoint
-DROP TABLE "maps_panel";--> statement-breakpoint
-DROP TABLE "map";--> statement-breakpoint
-DROP TABLE "universe";--> statement-breakpoint
-ALTER TABLE "profile" ADD COLUMN "githubId" text;
