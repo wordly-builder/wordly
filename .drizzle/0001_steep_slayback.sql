f0642aff9b92cf546aae5ecfdf23cb5fccb73ec5
Pebloop <pebloop@outlook.com>
2024-11-17 20:48:03 +0100

    Update drizzle migration files

---
+++ .drizzle/0001_steep_slayback.sql
@@ -0,0 +1,9 @@
+DROP TABLE "character_fields";--> statement-breakpoint
+DROP TABLE "characters_panel";--> statement-breakpoint
+DROP TABLE "characters_templates_field";--> statement-breakpoint
+DROP TABLE "characters_template";--> statement-breakpoint
+DROP TABLE "character";--> statement-breakpoint
+DROP TABLE "maps_panel";--> statement-breakpoint
+DROP TABLE "map";--> statement-breakpoint
+DROP TABLE "universe";--> statement-breakpoint
+ALTER TABLE "profile" ADD COLUMN "githubId" text;
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "drizzle-kit";
 import 'dotenv/config';
 
 export default {
-    schema: "./src/lib/database/schemas",
+    schema: "./src/lib/database/postgres/schemas",
     out: ".drizzle",
     driver: "pg",
     dbCredentials: {
