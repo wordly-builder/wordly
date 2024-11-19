c3bbc8c7d1dbf2408c9b9663e204b34eef26fe11
Pebloop <pebloop@outlook.com>
2024-11-19 23:12:26 +0100

    Add new workflow

--- src/lib/helpers/getProfileFromSession.ts
+++ src/lib/helpers/getProfileFromSession.ts
@@ -1,13 +1,13 @@
 "use server";
 
 import {postgres} from "../database/postgres/db";
-import { PROFILE_ID, FORCE_LOGIN } from "$env/static/private"
+import { env } from "$env/dynamic/private"
 import type {ProfileSelect} from "../database/postgres/schemas/profiles";
 
 export async function getProfileFromSession(session: any) : Promise<ProfileSelect | null> {
-    if (FORCE_LOGIN == 'true') {
-        if (PROFILE_ID != null) {
-            const profiles = await postgres.profiles.getById(+PROFILE_ID);
+    if (env.FORCE_LOGIN == 'true') {
+        if (env.PROFILE_ID != null) {
+            const profiles = await postgres.profiles.getById(+env.PROFILE_ID);
             if (profiles.length === 0) {
                 return null;
             }
